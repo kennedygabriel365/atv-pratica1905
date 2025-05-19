d7354e3236c63e541663fc41188a904d87e59fec
kennedygabriel365 <gabrielkennedy586@gmail.com>
2025-05-18 23:46:47 -0300

    atualizei

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ botao.innerHTML='Clique para ver uma mensagem';
 let contaCliques=0;
 
 botao.addEventListener('mouseover',e =>{
-    botao.style.background="lightgrey";
+    botao.style.background='lightgrey';
 });
 
 botao.addEventListener('mouseout',e =>{
     botao.style.background="#615e5e91";
 });
 
-botao.addEventListener("click",e =>{
+botao.addEventListener('click',e =>{
     
     contaCliques++;
     
     if(contaCliques%2===0){
-    botao.innerHTML="Clique para ver uma mensagem";
+    botao.innerHTML='Clique para ver uma mensagem';
     }
     else{
     botao.innerHTML='Olá, obrigado pela atenção, esse foi o meu primeiro trabalho no curso de analise e desenvolvimento de sistemas; espero que goste! ATÉ A PROXIMA👋😊';
